dc8a8a0566b9e828786f8d7e6ff1d19940912ab3
ezetabo <ezetabo78@gmail.com>
2023-10-11 12:54:18 -0300

    sodoku v1

--- src/app/juegos/propio/propio.component.ts
+++ src/app/juegos/propio/propio.component.ts
@@ -13,42 +13,59 @@ import Swal from 'sweetalert2';
 export class PropioComponent implements OnInit {
   public sudokuData!: Grid;
   public sudokuJugador!: Grid;
+  public solution: boolean = false;
 
   constructor(private sS: SodokuService) { }
 
   ngOnInit(): void {
+
+  }
+
+
+  newGame(): void {
     this.sS.getGrid().subscribe(tbl => {
       this.sudokuData = deepCopy(tbl);
       this.sudokuJugador = deepCopy(tbl);
+      this.sudokuData.solution?.forEach(x => console.log(x));
     });
   }
 
   check() {
-    console.log('matriz solucion')
-    this.sudokuData.solution!.forEach(x => console.log(x));
-
-    if (this.areMatricesEqual()) {
-      console.log('Las matrices son iguales');
-      Swal.fire({
-        position: 'center-end',
-        icon: 'success',
-        title: 'Felicitaciones Tablero CORRECTO',
-        showConfirmButton: false,
-        timer: 1500
-      });
-    } else {
-      console.log('Las matrices son distintas');
-      Swal.fire({
-        title: 'Oops...',
-        icon: 'error',
-        text: `"El tablero no es correcto"`
-      });
-    }
+    this.solution = !this.solution;
   }
 
 
-  isInitialValue(row: number, col: number): boolean {
-    return this.sudokuData.value[row][col] !== 0;
+
+  entregar(): void {
+
+    if (this.sudokuData) {
+      if (this.areMatricesEqual()) {
+        console.log('Las matrices son iguales');
+        Swal.fire({
+          position: 'center-end',
+          icon: 'success',
+          title: 'Felicitaciones Tablero CORRECTO',
+          showConfirmButton: false,
+          timer: 1500
+        });
+      } else {
+        console.log('Las matrices son distintas');
+        Swal.fire({
+          title: 'Oops...',
+          icon: 'error',
+          text: `"El tablero no es correcto"`
+        });
+      }
+    }
+  }
+
+  isInitialValue(row: number, col: number, solucion: boolean = false): boolean {
+
+    if (solucion) {
+      return this.sudokuData.solution![row][col] == this.sudokuJugador.value![row][col];
+    } else {
+      return this.sudokuData.value[row][col] !== 0;
+    }
   }
 
   areMatricesEqual(): boolean {
